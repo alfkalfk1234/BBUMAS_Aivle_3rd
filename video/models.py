d52fc0cdc95018830e061e0c14bf2da011a3cf16
Injae <alfkalfk1234@naver.com>
2023-06-19 17:35:38 +0900

    Yolov8 Test Module Integrated

--- video/models.py
+++ video/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    video_file = models.FileField(upload_to='')
--- video/urls.py
+++ video/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'video'
 urlpatterns = [
     path('', views.index, name='video'),
+    path('upload/', views.video_upload, name='video_upload'),
+    path('result/<str:result>/', views.video_result, name='video_result'),
 ]
