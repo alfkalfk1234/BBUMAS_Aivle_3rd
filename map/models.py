30455c58fa99cd8cc38cdf8dca664cd97f222244
awds1236 <awds1236@gmail.com>
2023-06-21 13:06:15 +0900

    map, video, post 데이터베이스 추가

--- map/models.py
+++ map/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Location(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    time = models.DateTimeField()
+    detected_object = models.CharField(max_length=200) # 검출물
--- map/views.py
+++ map/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core import serializers
+from .models import Location
 
 def index(request):
     return render(request, 'map/map.html')
+
+def get_locations(request):
+    locations = Location.objects.all()  # 모든 위치 데이터를 가져옵니다
+    location_list = serializers.serialize('json', locations)  # QuerySet을 JSON으로 변환
+    return JsonResponse(location_list, safe=False)  # JSON 응답 반환
