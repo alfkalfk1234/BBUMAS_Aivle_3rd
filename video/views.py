77cb332ef94684ec53a8928396aec2ffa50296e2
Injae_Lee <alfkalfk1234@naver.com>
2023-06-21 17:55:23 +0900

    yolov8 검출 내용(Frame, class 추출 및 검출 이미지 저장 등) 추가

--- video/views.py
+++ video/views.py
@@ -6,6 +6,8 @@ import subprocess
 import time
 import struct
 import pandas as pd
+import csv
+import cv2
 from datetime import datetime, timedelta
 
 def index(request):
@@ -16,7 +18,7 @@ def video_result(request):
 
 def analyze_video(video_path):
     # YOLOv8 analysis code
-    yolov8_model_path = 'video/tool/yolov8n.pt'
+    yolov8_model_path = 'video/tool/custom_yolov8m_0619.pt'
     model = YOLO(yolov8_model_path)
     model.predict(
         source = video_path,
@@ -25,9 +27,11 @@ def analyze_video(video_path):
         save = True,
         save_txt = True,
         project = 'video/media',
-        # classes = [0, 2, 3, 4, 5, 7, 8, 9, 10],
+        classes = [0, 2, 3, 4, 5, 7, 8, 9, 10],
+        # names: {0: 'bollard_abnormal', 1: 'bollard_normal', 2: 'crosswalk', 3: 'loading-box', 4: 'pothole', 5: 'road_separator', 6: 'road_separator_normal', 7: 'roadmarking', 8: 'speedbump', 9: 'tubular-marker-abnormal', 10: 'tubular-marker-normal'}
     )
     result = "YOLOv8 analysis result"
+
     return result
 
 def get_latlng(path,video_path):
@@ -36,8 +40,6 @@ def get_latlng(path,video_path):
     geo = []
     
     name,ext = os.path.splitext(video_path)
-    # createDir(f'{path}/bin')
-    # os.mkdir(f'{path}/bin')
     if ext =='.avi' or ext=='.mp4':
         file_name = name
 
@@ -49,7 +51,8 @@ def get_latlng(path,video_path):
         print(f'{output_file} 저장')
     else:
         print('등록된 확장자가 아닙니다.')
-    time.sleep(2)    
+
+    time.sleep(2)
     with open(f"{path}/bin/{file_name}.bin",'rb') as file:
         data = file.read()
         for i in range(0,len(data), 72):
@@ -78,9 +81,8 @@ def get_latlng(path,video_path):
         time_binarydata = geo_bin[2]
         time_converted_value = struct.unpack('<I', time_binarydata)[0]
         time_converted_value = datetime.fromtimestamp(time_converted_value)
-        formattime = time_converted_value.strftime('%Y년 %m월 %d일 %H시 %M분 %S초')
+        formattime = time_converted_value.strftime('%Y. %m. %d. %H. %M. %S')
         geo.append({'lat':round(lat_converted_value,6),'lng':round(lng_converted_value,6), 'time':formattime})
-    
         
     return geo
 
@@ -91,6 +93,7 @@ def extract_latitude_longitude(string):
     time = string['time']
     lat = convert_to_degrees(latitude)
     lng = convert_to_degrees(longitude)
+
     return lat, lng, time
 
 def convert_to_degrees(coord):
@@ -121,6 +124,7 @@ def extract_latitude_longitude(string):
     time = string['time']
     lat = convert_to_degrees(latitude)
     lng = convert_to_degrees(longitude)
+
     return lat, lng, time
 
 def convert_to_degrees(coord):
@@ -144,10 +148,57 @@ def convert_to_degrees(coord):
 
     return round(result,7)
 
-# def video_result(request, result):
-#     return render(request, 'video/video_result.html', {'result': result})
-def video_result(request):
-    return render(request, 'video/video_result.html')
+def save_images(video_file_name):
+    # 비디오 파일 열기
+    print(video_file_name)
+    video_path = f'video/media/predict/{video_file_name[:-4]}.mp4'
+    # video_path = f'video/media/predict/20230618-12h41m41s_N.mp4'
+    video_name = os.path.splitext(os.path.basename(video_path))[0]  # 비디오 파일의 이름 추출
+    video_capture = cv2.VideoCapture(video_path)
+
+    # 프레임 번호와 클래스 내용을 담을 딕셔너리
+    frame_info = {}
+
+    # 텍스트 파일들이 위치한 폴더 경로
+    txt_folder = 'video/media/predict/labels'
+
+    # 폴더 내의 모든 텍스트 파일을 검색하여 프레임 번호와 클래스 내용 추출
+    for file_name in os.listdir(txt_folder):
+        if file_name.endswith('.txt'):
+            txt_file = os.path.join(txt_folder, file_name)
+            frame_number = int(file_name.split('_')[-1].split('.')[0]) - 1  # 제목에서 프레임 번호 추출
+            with open(txt_file, 'r') as file:
+                class_name = file.readline().split()[0]  # 첫 번째 숫자를 클래스 내용으로 추출
+                frame_info[frame_number] = class_name
+
+    # 프레임 단위로 영상 캡쳐
+    frame_count = 0
+    info = []
+    while video_capture.isOpened():
+        ret, frame = video_capture.read()
+
+        if not ret:
+            break
+
+        if frame_count in frame_info:
+            class_name = frame_info[frame_count]  # 프레임 번호에 해당하는 클래스 내용 가져오기
+
+            # 특정 프레임에 대한 처리
+            # 예: 프레임 저장, 작업 수행 등
+            file_name = f'video/media/images/{video_name}_{frame_count+1}_{class_name}.jpg'
+            info.append({'frame' : frame_count+1, 'class_name' : class_name, 'file_name' : file_name})
+            cv2.imwrite(file_name, frame)
+            del frame_info[frame_count]  # 이미 처리한 프레임은 딕셔너리에서 제거
+
+            if not frame_info:  # 모든 프레임을 처리했으면 종료
+                break
+
+        frame_count += 1
+
+    # 비디오 캡처 종료
+    video_capture.release()
+    cv2.destroyAllWindows()
+    return info
 
 def video_upload(request):
     if request.method == 'POST' and request.FILES['video_file']:
@@ -162,6 +213,7 @@ def video_upload(request):
             for chunk in video_file.chunks():
                 file.write(chunk)
 
+        # 위치 정보 csv 파일로 저장
         path = 'video/media/'
         filename = video.video_file.name
         L = get_latlng(path, filename)
@@ -170,12 +222,40 @@ def video_upload(request):
             tmp = extract_latitude_longitude(latlng)
             LL.append(tmp)
         latlng = pd.DataFrame(LL, columns=['latitude', 'longitude', 'time'])
-        latlng.to_csv(f'video/media/csv/{video.video_file.name[:-4]}.csv', index=False, encoding='utf-8-sig')
+        length = len(LL)
+        second = [i for i in range(length)]
+        latlng['second'] = second
+        # latlng.to_csv(f'video/media/csv/{filename[:-4]}.csv', index=False, encoding='utf-8-sig')
 
         # yolov8 분석
-        result = analyze_video(video_path)
+        # result = analyze_video(video_path)
 
-        # return redirect('video:video_result', result=result)
-        return redirect('video:video_result')
+        # yolov8 분석 영상을 이미지로
+        info = save_images(filename) # frame, class_name, file_name
+        info = pd.DataFrame(info)
+        info['second'] = info['frame'] // 30
+        # 위치 정보 csv 파일 불러오기
+        # results = []
+        # with open(f'video/media/csv/{filename[:-4]}.csv', 'r', encoding='utf-8-sig') as file:
+        #     reader = csv.DictReader(file)
+        #     for row in reader:
+        #         results.append(row)
+        all = pd.merge(left = latlng , right = info, how = "inner", on = "second")
+        # all.to_csv(f'video/media/csv/results.csv', index=False, encoding='utf-8-sig')
+        all.loc[all["class_name"] == '0', "class_name"] = '볼라드 손상'
+        all.loc[all["class_name"] == '1', "class_name"] = '볼라드 정상'
+        all.loc[all["class_name"] == '2', "class_name"] = '횡단보도 손상'
+        all.loc[all["class_name"] == '3', "class_name"] = '제설함 손상'
+        all.loc[all["class_name"] == '4', "class_name"] = '포트홀'
+        all.loc[all["class_name"] == '5', "class_name"] = '도로 분리대 손상'
+        all.loc[all["class_name"] == '6', "class_name"] = '도로 분리대 정상'
+        all.loc[all["class_name"] == '7', "class_name"] = '도로 표지 손상'
+        all.loc[all["class_name"] == '8', "class_name"] = '방지턱 손상'
+        all.loc[all["class_name"] == '9', "class_name"] = '시선 유도봉 손상'
+        all.loc[all["class_name"] == '10', "class_name"] = '시선 유도봉 정상'
+        results = all.to_dict(orient='records')
+        context = {'results' : results}
+
+        return render(request, 'video/video_result.html', context)
     else:
         return render(request, 'video/video.html')
