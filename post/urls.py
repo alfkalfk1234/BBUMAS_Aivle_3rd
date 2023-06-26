c9c3bca62261ff7b41355cbb725f47cdeb6bc12b
awds1236 <awds1236@gmail.com>
2023-06-26 16:27:32 +0900

    게시물 사진 제거 수정, map post, video db 연결, video model 수정

--- post/urls.py
+++ post/urls.py
@@ -5,6 +5,7 @@ app_name = 'post'
 urlpatterns = [
     path('', views.index, name='post'),
     path('posting', views.posting, name='posting'),
-    path('<int:pk>', views.post_detail, name='post_detail'),    
+    path('<int:pk>', views.post_detail, name='post_detail'),   
+    path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name='post_delete'),
     path('faq', views.faq, name='faq'),
 ]
