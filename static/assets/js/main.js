fc7015daf203f534342cf38870198437f38343d2
KwakDaeunnn <nuead00@naver.com>
2023-07-06 14:08:04 +0900

    css 파일 분리 및 코드 정리

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,10 +1,3 @@
-/**
-* Template Name: Logis
-* Updated: May 30 2023 with Bootstrap v5.3.0
-* Template URL: https://bootstrapmade.com/logis-bootstrap-logistics-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
